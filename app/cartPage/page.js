355b2d36646d78fbc0512f2b222dae7aa2ab7633
Clara Mrkos <mrkos.clara@gmail.com>
2023-09-28 10:37:09 +0200

    Set up link navigation for cartPage

--- app/cartPage/page.js
+++ app/cartPage/page.js
@@ -1,25 +1,52 @@
+import Link from 'next/link';
+
 export default function CartPage() {
   return (
     <div className="cartpage">
       <h1 className="cart">Cart</h1>
 
-      <button className="itemsinCart"> </button>
+      <div className="ProductCard" data-test-id="cart-product-<product id>">
+        <div className="title">
+          <h2>Product.Name</h2>
+        </div>
 
-      <div className="ProductCard">
-        <div className="title">Product Name</div>
-
-        <p className="subtitle">{`Quantity: `}</p>
-        <div className="subtotalP1">Subtotal:</div>
-        <h2>Total Price:</h2>
+        <div
+          className="Quantity"
+          data-test-id="cart-product-quantity-<product id>"
+        >
+          <p>{`Quantity: `}</p>{' '}
+        </div>
+        <div className="subtotalP1">Product subtotal: price × quantity</div>
+        <p>Total Price:</p>
         <p>0</p>
-        <button className="buttonSecondary">Remove</button>
+        <button
+          className="buttonSecondary"
+          data-test-id="cart-product-remove-<product id>"
+        >
+          Remove
+        </button>
       </div>
 
       <div className="totalpriceandquantity">
-        <h2>Total Price:</h2>
-        <p>0</p>
+        <div className="totalprice">
+          <h2>Total Price of all products:</h2>
+          <p data-test-id="cart-total">0</p>
+        </div>
         <div className="quantity">{`Quantity: `}</div>
-        <button className="buttonSecondary">Checkout!</button>
+        <Link href="/shoppage">
+          <div className="buttonSecondary">
+            <div className="background3" />
+
+            <div className="cartbutton">Continue Shopping</div>
+          </div>
+        </Link>
+        <Link href="/checkoutPage">
+          <div className="buttonSecondary">
+            <div className="background3" />
+
+            <div className="cartbutton">Checkout</div>
+          </div>
+        </Link>
       </div>
     </div>
   );
