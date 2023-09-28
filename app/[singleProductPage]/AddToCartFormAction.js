d81373126326782aab1bf12f66401045411b4ad9
Clara Mrkos <mrkos.clara@gmail.com>
2023-09-28 15:08:28 +0200

    Set cookie cart functionality

--- app/[singleProductPage]/AddToCartFormAction.js
+++ app/[singleProductPage]/AddToCartFormAction.js
@@ -1,6 +1,77 @@
 'use server';
+
 import { cookies } from 'next/headers';
+import { getProductsByID } from '../../database/products';
 
 export async function setQuantityInCookies(singleProductID, quantityValue) {
-  await cookies().set('cart', quantityValue);
+  const singleProductFromDatabase = getProductsByID(singleProductID);
+  const cartCookie = cookies().get('cart')?.value;
+
+  if (cartCookie === undefined) {
+    cookies().set(
+      'cart',
+      JSON.stringify([
+        { id: singleProductFromDatabase.id, quantity: quantityValue },
+      ]),
+    );
+    console.log(
+      JSON.stringify([
+        { id: singleProductFromDatabase.id, quantity: quantityValue },
+      ]),
+    );
+  } else {
+    const parsedCartCookie = JSON.parse(cartCookie);
+    const singleProductToUpdate = parsedCartCookie.find(
+      (c) => c.id === singleProductID,
+    );
+
+    if (singleProductToUpdate) {
+      parsedCartCookie[
+        parsedCartCookie.indexOf(singleProductToUpdate)
+      ].quantity = quantityValue;
+      cookies().set('cart', JSON.stringify([...parsedCartCookie]));
+      console.log(JSON.stringify([...parsedCartCookie]));
+    } else {
+      parsedCartCookie.push({
+        id: singleProductFromDatabase.id,
+        quantity: quantityValue,
+      });
+      cookies().set('cart', JSON.stringify([...parsedCartCookie]));
+      console.log(JSON.stringify([...parsedCartCookie]));
+    }
+  }
+
+  /*
+
+  console.log(productExistsInCookieCart());
+
+  const singleProductToUpdate = parsedCartCookie.find(
+    (c) => c.id === singleProductID,
+  );
+
+  console.log(singleProductToUpdate);
+
+  await function setQuantityLogic() {
+      if (
+        productExistsInCookieCart(singleProductFromDatabase.id) ? true : false
+      ) {
+        parsedCartCookie[singleProductID].quantity = quantityValue;
+        console.log(parsedCartCookie);
+        cookies().set('cart', JSON.stringify([...parsedCartCookie]));
+      } else {
+        parsedCartCookie.push({
+          singleProductFromDatabase,
+          quantity: quantityValue,
+        });
+        cookies().set('cart', JSON.stringify([...parsedCartCookie]));
+      }
+    }
+
+    setQuantityLogic();
+
+  };
+
 }
+*/
+}
+await setQuantityInCookies;
