00fbe1d95f7b236fc2cd8552c39b0f3533aefaf2
Clara Mrkos <mrkos.clara@gmail.com>
2023-10-10 20:58:00 +0200

    Update code to fit types

--- database/products.tsx
+++ database/products.tsx
@@ -10,7 +10,6 @@ type Product = {
 };
 
 // get database sql;
-
 export const getProductsSQL = cache(async () => {
   const products = await sql<Product[]>`
     SELECT * FROM products
@@ -30,46 +29,3 @@ export const getProductSQLById = cache(async (id: number) => {
   `;
   return product;
 });
-/*
-
-// use hardcoded database in project
-
-export const products = [
-  {
-    id: 1,
-    name: 'Kinder',
-    price: 1,
-    image:
-      'https://familienausflug.info/img/4567/ausflugsziel-hermannsh%C3%B6hle-17088.jpg',
-  },
-  {
-    id: 2,
-    name: 'Ermäßigt',
-    price: 2,
-    image:
-      'https://www.wieneralpen.at/data/_wieneralpen/mediadb/cms_pictures/%7B14eb1d3d-b983-804c-8bea-18aca08e4510%7D.jpeg',
-  },
-  {
-    id: 3,
-    name: 'Gruppenmitglied',
-    price: 2.5,
-    image:
-      'https://cdn.1000things.at/app/uploads/Bildschirmfoto-2018-03-27-um-14.20.11.png',
-  },
-  {
-    id: 4,
-    name: 'Erwachsene/-r',
-    price: 3,
-    image:
-      'https://vcdn.bergfex.at/images/resized/profiles/detail/fe6/4ee19122d218070c16f9a5bfdcdebfe6.jpg?1592293387',
-  },
-];
-
-export function getProducts() {
-  return products;
-}
-
-export function getProductsByID(id:number) {
-  return products.find((p) => p.id === id);
-}
-*/
