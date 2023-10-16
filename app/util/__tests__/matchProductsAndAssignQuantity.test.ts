71cbbef86ab6ffe02c7258467f3efb0b79dd45e0
Clara Mrkos <mrkos.clara@gmail.com>
2023-10-16 16:21:58 +0200

    Fix preflight warnings

--- app/util/__tests__/matchProductsAndAssignQuantity.test.ts
+++ app/util/__tests__/matchProductsAndAssignQuantity.test.ts
@@ -1,15 +1,39 @@
 import { expect, test } from '@jest/globals';
-import { calculateQuantityInCookiesAlreadyExisting } from '../cookieValueFunction';
+import { Product } from '../../../migrations/00000-createTableProducts';
+import { matchProductsAndAssignQuantity } from '../matchAndAssignQuantityFunction';
 
 // test database products
 
-const testProduct1 = {
-  id: 1,
-  name: 'Kinder',
-  price: 1,
-  image:
-    'https://image.jimcdn.com/app/cms/image/transf/dimension=2048x2048:format=jpg/path/sfa1e985dff417ba4/image/i707c3e4fc445bf98/version/1646756208/image.jpg',
-};
+const testProducts: Product[] = [
+  {
+    id: 1,
+    name: 'Kinder',
+    price: 1,
+    image:
+      'https://image.jimcdn.com/app/cms/image/transf/dimension=2048x2048:format=jpg/path/sfa1e985dff417ba4/image/i707c3e4fc445bf98/version/1646756208/image.jpg',
+  },
+  {
+    id: 2,
+    name: 'Ermäßigt',
+    price: 2,
+    image:
+      'https://image.jimcdn.com/app/cms/image/transf/dimension=2048x2048:format=jpg/path/sfa1e985dff417ba4/image/ic5bad08a24720915/version/1646756758/image.jpg',
+  },
+  {
+    id: 3,
+    name: 'Gruppenmitglied',
+    price: 2.5,
+    image:
+      'https://image.jimcdn.com/app/cms/image/transf/dimension=2048x2048:format=jpg/path/sfa1e985dff417ba4/image/i96c59e6a0dffe07f/version/1646756067/image.jpg',
+  },
+  {
+    id: 4,
+    name: 'Erwachsene/-r',
+    price: 3,
+    image:
+      'https://image.jimcdn.com/app/cms/image/transf/dimension=2048x2048:format=jpg/path/sfa1e985dff417ba4/image/icb92504cc82f773e/version/1646756104/image.jpg',
+  },
+];
 
 // test values test 1
 export type Cookie = {
@@ -19,21 +43,34 @@ export type Cookie = {
 
 const testCookieArray1: Cookie[] = [
   { id: 1, quantity: 4 },
-  { id: 3, quantity: 6 },
+  { id: 4, quantity: 1 },
 ];
-const addedProduct1 = { id: 1, quantity: 2 };
 
-const expectedCookieArray1 = [
-  { id: 1, quantity: 6 },
-  { id: 3, quantity: 6 },
+const expectedProductArray1 = [
+  {
+    id: 1,
+    name: 'Kinder',
+    price: 1,
+    image:
+      'https://image.jimcdn.com/app/cms/image/transf/dimension=2048x2048:format=jpg/path/sfa1e985dff417ba4/image/i707c3e4fc445bf98/version/1646756208/image.jpg',
+    quantity: 4,
+  },
+  {
+    id: 4,
+    name: 'Erwachsene/-r',
+    price: 3,
+    image:
+      'https://image.jimcdn.com/app/cms/image/transf/dimension=2048x2048:format=jpg/path/sfa1e985dff417ba4/image/icb92504cc82f773e/version/1646756104/image.jpg',
+    quantity: 1,
+  },
 ];
-const expectedCookieArrayString1 = JSON.stringify(expectedCookieArray1);
+
+// const expectedProductArrayString1 = JSON.stringify(expectedProductArray1);
 
 test('calculateQuantityInCookiesAlreadyExisting', async () => {
-  const cookieValueTest = await calculateQuantityInCookiesAlreadyExisting(
-    Number(testProduct1.id),
-    Number(addedProduct1.quantity),
+  const cookieValueTest = await matchProductsAndAssignQuantity(
+    testProducts,
     testCookieArray1,
   );
-  expect(cookieValueTest).toBe(expectedCookieArrayString1);
+  expect(cookieValueTest).toStrictEqual(expectedProductArray1);
 });
--- app/util/__tests__/testCalculateCookieValue.test.ts
+++ app/util/__tests__/testCalculateCookieValue.test.ts
@@ -1,39 +1,15 @@
 import { expect, test } from '@jest/globals';
-import { Product } from '../../../migrations/00000-createTableProducts';
-import { matchProductsAndAssignQuantity } from '../matchAndAssignQuantityFunction';
+import { calculateQuantityInCookiesAlreadyExisting } from '../cookieValueFunction';
 
 // test database products
 
-const testProducts: Product[] = [
-  {
-    id: 1,
-    name: 'Kinder',
-    price: 1,
-    image:
-      'https://image.jimcdn.com/app/cms/image/transf/dimension=2048x2048:format=jpg/path/sfa1e985dff417ba4/image/i707c3e4fc445bf98/version/1646756208/image.jpg',
-  },
-  {
-    id: 2,
-    name: 'Ermäßigt',
-    price: 2,
-    image:
-      'https://image.jimcdn.com/app/cms/image/transf/dimension=2048x2048:format=jpg/path/sfa1e985dff417ba4/image/ic5bad08a24720915/version/1646756758/image.jpg',
-  },
-  {
-    id: 3,
-    name: 'Gruppenmitglied',
-    price: 2.5,
-    image:
-      'https://image.jimcdn.com/app/cms/image/transf/dimension=2048x2048:format=jpg/path/sfa1e985dff417ba4/image/i96c59e6a0dffe07f/version/1646756067/image.jpg',
-  },
-  {
-    id: 4,
-    name: 'Erwachsene/-r',
-    price: 3,
-    image:
-      'https://image.jimcdn.com/app/cms/image/transf/dimension=2048x2048:format=jpg/path/sfa1e985dff417ba4/image/icb92504cc82f773e/version/1646756104/image.jpg',
-  },
-];
+const testProduct1 = {
+  id: 1,
+  name: 'Kinder',
+  price: 1,
+  image:
+    'https://image.jimcdn.com/app/cms/image/transf/dimension=2048x2048:format=jpg/path/sfa1e985dff417ba4/image/i707c3e4fc445bf98/version/1646756208/image.jpg',
+};
 
 // test values test 1
 export type Cookie = {
@@ -43,34 +19,21 @@ export type Cookie = {
 
 const testCookieArray1: Cookie[] = [
   { id: 1, quantity: 4 },
-  { id: 4, quantity: 1 },
+  { id: 3, quantity: 6 },
 ];
+const addedProduct1 = { id: 1, quantity: 2 };
 
-const expectedProductArray1 = [
-  {
-    id: 1,
-    name: 'Kinder',
-    price: 1,
-    image:
-      'https://image.jimcdn.com/app/cms/image/transf/dimension=2048x2048:format=jpg/path/sfa1e985dff417ba4/image/i707c3e4fc445bf98/version/1646756208/image.jpg',
-    quantity: 4,
-  },
-  {
-    id: 4,
-    name: 'Erwachsene/-r',
-    price: 3,
-    image:
-      'https://image.jimcdn.com/app/cms/image/transf/dimension=2048x2048:format=jpg/path/sfa1e985dff417ba4/image/icb92504cc82f773e/version/1646756104/image.jpg',
-    quantity: 1,
-  },
+const expectedCookieArray1 = [
+  { id: 1, quantity: 6 },
+  { id: 3, quantity: 6 },
 ];
-
-// const expectedProductArrayString1 = JSON.stringify(expectedProductArray1);
+const expectedCookieArrayString1 = JSON.stringify(expectedCookieArray1);
 
 test('calculateQuantityInCookiesAlreadyExisting', async () => {
-  const cookieValueTest = await matchProductsAndAssignQuantity(
-    testProducts,
+  const cookieValueTest = await calculateQuantityInCookiesAlreadyExisting(
+    Number(testProduct1.id),
+    Number(addedProduct1.quantity),
     testCookieArray1,
   );
-  expect(cookieValueTest).toStrictEqual(expectedProductArray1);
+  expect(cookieValueTest).toBe(expectedCookieArrayString1);
 });
