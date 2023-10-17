ab6b13149daaf76a44b03ad3b426c217243ebd4b
Clara Mrkos <mrkos.clara@gmail.com>
2023-10-17 10:17:28 +0200

    Fix stack overflow error - called function in itself

--- app/cart/CartRemoveAction.js
+++ app/cart/CartRemoveAction.js
@@ -1,21 +1,23 @@
 'use server';
 
 import { cookies } from 'next/headers';
-import { getParsedCookie } from '../util/getCookie';
 
 export async function removeItemFromCookies(props) {
-  const parsedCartCookie = getParsedCookie();
+  const cartCookie = cookies().get('cart')?.value;
+  const parsedCartCookie =
+    !cartCookie || JSON.parse(cartCookie).length === 0
+      ? []
+      : JSON.parse(cartCookie);
 
-  if (props === undefined) {
-    console.log('no id to remove');
-  } else {
+  if (props !== undefined) {
     const singleProductToUpdateIndex = await parsedCartCookie.indexOf(
       parsedCartCookie.find((c) => c.id === props),
     );
+
+    console.log('updated array:' + singleProductToUpdateIndex);
+
     await parsedCartCookie.splice(Number(singleProductToUpdateIndex), 1);
-    cookies().set('cart', JSON.stringify([...parsedCartCookie]));
+    await cookies().set('cart', JSON.stringify([...parsedCartCookie]));
   }
-  removeItemFromCookies().catch((error) => {
-    console.log(error);
-  });
+
 }
